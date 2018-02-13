6c257789291f802569f9e7a99d49ee1637e19f8f
Iksan Maricar <iksansahib@gmail.com>
2018-02-14 01:44:39 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view_engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Welcome to my web',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000);
